8775f74f31121a3ffa2d176a935293f29f3e2557
(Sammy) Samaru Ebinne <51972663+sammyebinne@users.noreply.github.com>
2024-07-07 00:23:03 -0600

    adds GET method to the HttpUtil to separate fetch logic

--- src/main/java/com/myapp/utils/HttpUtli.java
+++ src/main/java/com/myapp/utils/HttpUtli.java
@@ -1,5 +1,37 @@
 package com.myapp.utils;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class HttpUtli {
+	// Static method to fetch data from the URL using a GET request
+	// it takes in the url string and returns the JSON object as a string
+	public static String fetchData(String urlString) {
+		// we use a try catch block to handle possible runtime exceptions
+		try {
+			// we create a HttpClient instance using Java 11 HTTP Client API
+			HttpClient client = HttpClient.newHttpClient();
+			// build HTTP request object
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(new URI(urlString)) // set the URI to urlString
+					.GET() // specify GET as the request type
+					.build(); // build the object
+
+			// we send the request and receive the response then convert the response body to a String
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+			// if statusCode for response is 200, we're successful with the request and can return it
+			if (response.statusCode() == 200) {
+				return response.body();
+			} else { // else, we want to throw the runtime exception with a custom message about the statusCode
+				throw new RuntimeException("GET request failed, response code: " + response.statusCode());
+			}
+
+		} catch (Exception e) { // the catch block then gives us the original thrown exception
+			throw new RuntimeException("Error while making GET request", e);
+		}
+	}
 
 }
---
+++ src/main/java/com/myapp/views/MainView.java
@@ -0,0 +1,10 @@
+package com.myapp.views;
+
+public class MainView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
